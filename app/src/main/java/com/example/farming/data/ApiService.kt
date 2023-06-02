63c42d3e6d7f594e200ccf3eebdbeecf74e8cbb1
MuindiStephen <stephenmuindi241@gmail.com>
2023-06-02 08:33:03 +0300

    baseUrl: inputs

--- app/src/main/java/com/example/farming/data/ApiService.kt
+++ app/src/main/java/com/example/farming/data/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 
 interface ApiService {
-     @GET("supplies")
+     @GET("inputs")
      fun getAllSuppliers() : Call<ArrayList<SuppliersDTOItem>>
 }
--- app/src/main/java/com/example/farming/utils/Constants.kt
+++ app/src/main/java/com/example/farming/utils/Constants.kt
@@ -1,3 +1,3 @@
 package com.example.farming.utils
 
-const val MOBILE_KILIMO_BASE_URL = "https://testapi.io/api/stevemd/https://testapi.io/api/mobilekilimo/"
+const val MOBILE_KILIMO_BASE_URL = "https://testapi.io/api/stevemd/"
