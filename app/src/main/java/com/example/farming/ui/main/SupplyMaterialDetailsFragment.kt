b17440d46172796ec4da701993256ce785a05231
MuindiStephen <stephenmuindi241@gmail.com>
2023-06-05 14:23:20 +0300

    implemented navigation by navArgs

--- app/src/main/java/com/example/farming/ui/main/SupplyMaterialDetailsFragment.kt
+++ app/src/main/java/com/example/farming/ui/main/SupplyMaterialDetailsFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.farming.R
 import com.example.farming.databinding.FragmentSupplyMaterialDetailsBinding
 
 class SupplyMaterialDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentSupplyMaterialDetailsBinding
+
+    private val args:SupplyMaterialDetailsFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +26,26 @@ class SupplyMaterialDetailsFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val suppliersItem = args.suppliersitem
+
+        binding.apply {
+            Glide.with(requireContext())
+                .load(suppliersItem.image)
+                .into(materialImage)
+            materialSupplyText.text = suppliersItem.materialSupply
+            priceText.text = suppliersItem.price
+            supplierText.text = suppliersItem.supplier
+            ratingText.text = suppliersItem.rating
+        }
+
+        setUpBinding()
+    }
+
+    private fun setUpBinding() {
+        binding.buttonCheckout.setOnClickListener {
+            findNavController().navigate(R.id.action_supplyMaterialDetailsFragment_to_itemBiddingDetailFragment2)
+        }
     }
 
 }
